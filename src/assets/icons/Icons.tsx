aaadb69be7dea3c905c8b503899be2520aac2b72
grooveIsAll <bobby@backer.com>
2025-08-10 13:06:03 -0700

    create media queries for desktop and mobile, mobile nav, cleanup

--- src/assets/icons/Icons.tsx
+++ src/assets/icons/Icons.tsx
@@ -1,8 +1,8 @@
-import instagramIcon from './ig.svg';
-import spotifyIcon from './spotify.svg';
-import youtubeIcon from './youtube.svg';
-import webIcon from './www.svg';
-import soundcloudIcon from './soundcloud.svg';
+import instagramIcon from './ig.png';
+import spotifyIcon from './spotify.png';
+import youtubeIcon from './youtube.png';
+import webIcon from './www.png';
+import soundcloudIcon from './soundcloud.png';
 import facebookIcon from './fb-dark.png';
 import instagramDarkIcon from './ig-dark.png';
 
--- src/components/shared/Nav/Nav.tsx
+++ src/components/shared/Nav/Nav.tsx
@@ -1,13 +1,17 @@
-import ClickableIcon from '../ClickableIcon/ClickableIcon';
+import { useState } from "react";
 
-import { Icons } from '../../../assets/icons/Icons';
-import mustacheLogo from '../../../assets/page-imgs/mustache.jpg';
-import styles from './Nav.module.scss';
+import ClickableIcon from "../ClickableIcon/ClickableIcon";
+
+import { Icons } from "../../../assets/icons/Icons";
+import mustacheLogo from "../../../assets/page-imgs/mustache.jpg";
+import styles from "./Nav.module.scss";
 
 const { facebook, instagramDark } = Icons;
-const googleLink = 'https://google.com';
+const googleLink = "https://google.com";
 
 const Nav = () => {
+  const [menuOpen, setMenuOpen] = useState(false);
+
   return (
     <nav className={styles.nav}>
       <div className={styles.logoContainer}>
@@ -18,65 +22,150 @@ const Nav = () => {
           styleConfig={{ imgClass: styles.logo }}
         />
         <span className={styles.logoText}>
-          Upcoming bash
-          in San Diego
-          March 23, 2024
+          Upcoming bash in San Diego March 23, 2024
         </span>
       </div>
-      <div className={styles.linksContainer}>
-        <div className={styles.linksRight}>
-          <a
-            className={styles.link}
-            href={googleLink}
-            target="_blank"
-            rel="noreferrer"
-          >
-              San Diego</a>
-          <a
-            className={styles.link}
-            href={googleLink}
-            target="_blank"
-            rel="noreferrer"
-          >
-              San Francisco</a>
-          <a
-            className={styles.link}
-            href={googleLink}
-            target="_blank"
-            rel="noreferrer"
-          >
-              Gallery</a>
-          <a
-            className={styles.link}
-            href={googleLink}
-            target="_blank"
-            rel="noreferrer"
-          >
-              Info</a>
-        </div>
-        <div className={styles.linksLeft}>
-          <a
-            className={styles.ticketButton}
-            href={googleLink}
-            target="_blank"
-            rel="noreferrer">
-            TICKETS SOON
-          </a>
-          <ClickableIcon
-            icon={facebook}
-            url={googleLink}
-            styleConfig={{ imgClass: styles.socialIcon }}
-          />
-
-          <ClickableIcon
-            icon={instagramDark}
-            url={googleLink}
-            styleConfig={{ imgClass: styles.socialIcon }}
-          />
-        </div>
+      <DesktopLinks />
+      <button
+        className={styles.menuButton}
+        onClick={() => setMenuOpen((prev) => !prev)}
+        aria-label="Open menu"
+      >
+        <span className={styles.menuIcon}>X</span>
+      </button>
+      <div className={`${styles.mobileOverlay} ${menuOpen ? styles.open : ""}`}>
+        <button
+          className={styles.closeButton}
+          onClick={() => setMenuOpen(false)}
+          aria-label="Close menu"
+        >
+          ×
+        </button>
+        <MobileLinks />
       </div>
     </nav>
   );
 };
 
 export default Nav;
+
+const DesktopLinks = () => {
+  return (
+    <div className={styles.linksContainer}>
+      <div className={styles.linksLeft}>
+        <a
+          className={styles.link}
+          href={googleLink}
+          target="_blank"
+          rel="noreferrer"
+        >
+          San Diego
+        </a>
+        <a
+          className={styles.link}
+          href={googleLink}
+          target="_blank"
+          rel="noreferrer"
+        >
+          San Francisco
+        </a>
+        <a
+          className={styles.link}
+          href={googleLink}
+          target="_blank"
+          rel="noreferrer"
+        >
+          Gallery
+        </a>
+        <a
+          className={styles.link}
+          href={googleLink}
+          target="_blank"
+          rel="noreferrer"
+        >
+          Info
+        </a>
+      </div>
+      <div className={styles.linksRight}>
+        <a
+          className={styles.ticketButton}
+          href={googleLink}
+          target="_blank"
+          rel="noreferrer"
+        >
+          TICKETS SOON
+        </a>
+        <ClickableIcon
+          icon={facebook}
+          url={googleLink}
+          styleConfig={{ imgClass: styles.socialIcon }}
+        />
+
+        <ClickableIcon
+          icon={instagramDark}
+          url={googleLink}
+          styleConfig={{ imgClass: styles.socialIcon }}
+        />
+      </div>
+    </div>
+  );
+};
+
+const MobileLinks = () => {
+  return (
+    <div className={styles.overlayLinks}>
+      <a
+        className={styles.link}
+        href={googleLink}
+        target="_blank"
+        rel="noreferrer"
+      >
+        San Diego
+      </a>
+      <a
+        className={styles.link}
+        href={googleLink}
+        target="_blank"
+        rel="noreferrer"
+      >
+        San Francisco
+      </a>
+      <a
+        className={styles.link}
+        href={googleLink}
+        target="_blank"
+        rel="noreferrer"
+      >
+        Gallery
+      </a>
+      <a
+        className={styles.link}
+        href={googleLink}
+        target="_blank"
+        rel="noreferrer"
+      >
+        Info
+      </a>
+      <a
+        className={styles.ticketButton}
+        href={googleLink}
+        target="_blank"
+        rel="noreferrer"
+      >
+        TICKETS SOON
+      </a>
+      <div className={styles.overlaySocials}>
+        <ClickableIcon
+          icon={facebook}
+          url={googleLink}
+          styleConfig={{ imgClass: styles.socialIcon }}
+        />
+        <ClickableIcon
+          icon={instagramDark}
+          url={googleLink}
+          styleConfig={{ imgClass: styles.socialIcon }}
+        />
+      </div>
+    </div>
+  );
+};
